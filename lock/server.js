a264793581e4874d5f0382e5a93c83631723ead8
Felipe Lalanne <felipe@balena.io>
2024-09-05 16:25:32 -0400

    Call the supervisor update endpoint on unlock

--- lock/server.js
+++ lock/server.js
@@ -5,6 +5,7 @@ const lockfile = require('lockfile');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const app = express();
+const http = require('http');
 
 const staticPath = path.resolve(__dirname, 'static');
 const lockPath = '/tmp/balena/updates.lock';
@@ -12,6 +13,46 @@ const lockPath = '/tmp/balena/updates.lock';
 app.use(express.static(staticPath));
 app.use(express.json());
 app.use(cors());
+
+const SV_ADDR = process.env.BALENA_SUPERVISOR_ADDRESS;
+const SV_KEY = process.env.BALENA_SUPERVISOR_API_KEY;
+
+function triggerUpdate() {
+    if (SV_ADDR == null) {
+        return;
+    }
+    const url = new URL(`${SV_ADDR}/v1/update?apikey=${SV_KEY}`);
+    const data = JSON.stringify({});
+    const req = http
+        .request(
+            url,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Content-Length': Buffer.byteLength(data),
+                },
+            },
+            (res) => {
+                let data = '';
+
+                // A chunk of data has been received.
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                res.on('end', () => {
+                    console.log(data);
+                });
+            },
+        )
+        .on('error', (err) => {
+            console.log('Error: ' + err.message);
+        });
+    req.write(data);
+    req.end();
+}
 
 app.post('/lock', (req, res) => {
     lockfile.lock(lockPath, (err) => {
@@ -29,6 +70,7 @@ app.post('/unlock', (req, res) => {
             console.error(err);
             return res.send(err);
         }
+        triggerUpdate();
         res.status(201).send('Lockfile removed');
     });
 });
@@ -52,8 +94,9 @@ app.post('/count', async (req, res) => {
 app.get('/:service/count', async (req, res) => {
     const { service } = req.params;
     const port = service === 'one' ? 3000 : 3001;
-    const countResponse = await fetch(`http://${service}:${port}/count`, { method: 'GET' })
-        .then(rsp => rsp.json());
+    const countResponse = await fetch(`http://${service}:${port}/count`, {
+        method: 'GET',
+    }).then((rsp) => rsp.json());
     res.status(200).json(countResponse);
 });
 
